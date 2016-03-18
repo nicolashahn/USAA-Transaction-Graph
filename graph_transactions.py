cbada6351d1b9f0d6033072f6447a72888fa68db
Nicolas Hahn <nicolas@stonespring.org>
2016-03-18 09:59:42 -0700

    x axis shows dates now

--- graph_transactions.py
+++ graph_transactions.py
@@ -6,42 +6,42 @@
 
 import csv
 import sys
+import datetime
 import matplotlib.pyplot as plt
+from matplotlib.dates import YearLocator, MonthLocator, DateFormatter
 
 class Transaction:
     '''store necessary information for a single transaction'''
     
-    def __init__(self,date,name,amount):
-        self.date = date
+    def __init__(self,raw_date,name,amount):
+        month, day, year = tuple(raw_date.split('/'))
+        self.date = datetime.date(int(year), int(month), int(day))
         self.name = name
         self.amount = float(amount.replace('--',''))
 
     def __str__(self):
-        return self.date+', '+self.name+', '+str(self.amount)
+        return str(self.date)+', '+self.name+', '+str(self.amount)
 
 
 def graph_transactions(transaction_list, date_total=0):
     '''
     take a list of transaction objects, create a graph
-    date_total = starting balance of account
+    date_total = current balance of account
     '''
     
     transaction_list_sorted = sorted(transaction_list, key=lambda x: x.date)
     date_totals = []
     curr_date = transaction_list_sorted[0].date
-    # the number of dates after the first date
-    t_plus_date = 0
     for transaction in transaction_list_sorted:
         if transaction.date == curr_date:
             date_total += transaction.amount
         else:
-            date_totals.append((curr_date, date_total, t_plus_date))
+            date_totals.append((curr_date, date_total))
             curr_date = transaction.date
-            t_plus_date += 1
             date_total += transaction.amount
-    t_plus_dates = [date_total[2] for date_total in date_totals]
+    dates = [date_total[0] for date_total in date_totals]
     amounts = [date_total[1] for date_total in date_totals]
-    plt.plot(t_plus_dates, amounts)
+    plt.plot(dates, amounts)
     plt.show()
 
 
