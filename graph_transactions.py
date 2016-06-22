0d3e1dd0b3de7e0457471eaf3180d3116cbf6f3a
Nicolas Hahn <nicolas@stonespring.org>
2016-06-22 09:13:08 -0700

    allow user to specify period

--- graph_transactions.py
+++ graph_transactions.py
@@ -13,7 +13,9 @@ import matplotlib.pyplot as plt
 from matplotlib.dates import YearLocator, MonthLocator, DateFormatter
 
 # default how many days back to calculate balance in get_period_balance()
-PERIOD = 7
+period = 7
+# current balance after all transactions, used to show accurate account balances for all transactions
+curr_balance = 0
 
 class Transaction:
     '''store necessary information for a single transaction'''
@@ -28,9 +30,10 @@ class Transaction:
         return str(self.date)+', '+self.name+', '+str(self.amount)
 
 
-def get_period_balance(transaction_list, period=PERIOD):
+def get_period_balance(transaction_list, period=period):
     '''takes transaction list and list of dates in a period and calculates balance'''
-    
+   
+    #TODO: this currently uses the current date, have it use the last date in the transaction list
     today = datetime.datetime.now()
     period_dates = []
     for day in range(1,period+1):
@@ -41,6 +44,7 @@ def get_period_balance(transaction_list, period=PERIOD):
         if transaction.date in period_dates:
             balance -= transaction.amount
     return balance
+
 
 def graph_transactions(transaction_list, date_total=0):
     '''
@@ -58,23 +62,27 @@ def graph_transactions(transaction_list, date_total=0):
             date_totals.append((curr_date, date_total))
             curr_date = transaction.date
             date_total += transaction.amount
-    dates = [date_total[0] for date_total in date_totals]
-    amounts = [date_total[1] for date_total in date_totals]
+    dates = [dt[0] for dt in date_totals]
+    amounts = [dt[1] for dt in date_totals]
     plt.rcParams["figure.figsize"] = (14,8)
     plt.plot(dates, amounts)
     plt.show()
 
+
 def main(transactions_csv, curr_balance):
+
     with open(transactions_csv, 'r') as file:
         csv_lines = csv.reader(file.readlines())
         transactions = [Transaction(line[2],line[4],line[6]) for line in csv_lines if len(line) == 7]
-    print('Your net balance is $'+str(get_period_balance(transactions, PERIOD))+
-          ' for the last '+str(PERIOD)+' days.')
+    print('Your net balance is $'+str(get_period_balance(transactions, period))+
+          ' for the last '+str(period)+' days.')
     graph_transactions(transactions, curr_balance)
 
+
 if __name__ == "__main__":
+
     transactions_csv = sys.argv[1]
-    if len(sys.argv) == 3: curr_balance = float(sys.argv[2])
-    else: curr_balance = 0
+    if len(sys.argv) >= 3: curr_balance = float(sys.argv[2])
+    if len(sys.argv) >= 4: period = int(sys.argv[3])
     main(transactions_csv, curr_balance)
 
